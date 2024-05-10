4e06f26f0b4b3d7236182acb58068fd79c0e9628
Nguyễn Nghiệp <nghiep2k3>
2024-05-10 15:43:46 +0700

    end lesson

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 // Chuyển hướng đến trang form_login.php
-header("Location: form_login.php");
-exit; // Đảm bảo không có mã PHP hoặc HTML nào được thực thi sau lệnh header
+header("Location: ./NghiepKtgk/view/form_login.php");
+exit;
 ?>
