7e1f5a3876f5f2d563257bcbe25d46e896e4c0a5
VinhT3rB0 <vinhvinh132465@gmail.com>
2024-05-10 15:08:30 +0700

    fixlogin

--- Vinh_ktra_giua_ki/controller/LoginController.php
+++ Vinh_ktra_giua_ki/controller/LoginController.php
@@ -1,5 +1,5 @@
 <?php
-        include "config/connect.php";
+        include "../config/connect.php";
         session_start();
         if(isset($_POST['login'])) {
             $username = $_POST['username'];
@@ -8,10 +8,10 @@
             $result = mysqli_query($conn,$sql);
             if(mysqli_num_rows($result)==1) {
                 $_SESSION['mySession'] = $username;
-                header("location:Sach.php");
+                header("location: ../Sach.php");
             }
             else {
                 echo "Sai tài khoản hoặc mật khẩu";
             }
         }
-    ?>
+?>
